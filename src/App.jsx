bbff64ebdf1c117cad0bb1f6af6b9ba01bf9aff9
German Gambera <germangambera@gmail.com>
2025-03-27 23:24:42 -0400

    Tab format changed

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,13 @@ import TodoForm from './TodoForm'
 import TodoList from './TodoList'
 
 function App() {
-    return (
-        <div>
-            <h1>Todo List</h1>
-            <TodoForm></TodoForm>
-            <TodoList></TodoList>
-        </div>
-    )
+  return (
+    <div>
+      <h1>Todo List</h1>
+      <TodoForm></TodoForm>
+      <TodoList></TodoList>
+    </div>
+  )
 }
 
 export default App
