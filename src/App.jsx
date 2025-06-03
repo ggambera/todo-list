ea43748a531b682b6f06ebf1287b188a98bae99d
ggambera <germangambera@gmail.com>
2025-06-03 12:25:55 -0400

    Code commented and unneccesary import removed

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,7 @@ import logo from './assets/todo.png'
 import {
   reducer as todosReducer,
   actions as todoActions,
-  initialState as initialTodosState,
-  actions,
+  initialState as initialTodosState
 } from './reducers/todos.reducer';
 
 function App() {
@@ -46,38 +45,16 @@ function App() {
         },
       };
       try {
-        /* Before reducer
-        setIsLoading(true); 
-        */
         dispatch({ type: todoActions.fetchTodos });
         const resp = await fetch(encodeUrl(sortField, sortDirection, queryString), options);
         if (!resp.ok) {
           throw new Error(resp.message);
         }
         const { records } = await resp.json();
-        /* Before reducer
-        setTodoList(records.map((record) => {
-          const todo = {
-            id: record.id,
-            ...record.fields,
-          };
-          if (!todo.isCompleted) {
-            todo.isCompleted = false;
-          }
-          return todo;
-        }));
-        */
         dispatch({ type: todoActions.loadTodos, records: records });
       } catch (error) {
-        /* Before reducer
-        console.error(error);
-        setErrorMessage(error.message);
-        */
         dispatch({ type: todoActions.setLoadError, error: error });
       } finally {
-        /* Before reducer 
-        setIsLoading(false);
-        */
         dispatch({ type: todoActions.endRequest });
       }
     })();
@@ -104,36 +81,16 @@ function App() {
       body: JSON.stringify(payload),
     };
     try {
-      /* Before reducer
-      setIsSaving(true);
-      */
       dispatch({ type: todoActions.startRequest });
       const resp = await fetch(encodeUrl(sortField, sortDirection, queryString), options);
       if (!resp.ok) {
         throw new Error(resp.message);
       }
       const { records } = await resp.json();
-      /* Before reducer
-      const savedTodo = {
-        id: records[0].id,
-        ...records[0].fields,
-      };
-      if (!records[0].fields.isCompleted) {
-        savedTodo.isCompleted = false;
-      }
-      setTodoList([...todoList, savedTodo]);
-      */
       dispatch({ type: todoActions.addTodo, records: records });
     } catch (error) {
-      /* Before reducer
-      console.error(error);
-      setErrorMessage(error.message);
-      */
       dispatch({ type: todoActions.setLoadError, error: error });
     } finally {
-      /* Before reducer
-      setIsSaving(false);
-      */
       dispatch({ type: todoActions.endRequest });
     }
   }
@@ -142,16 +99,6 @@ function App() {
     console.log('App: updateTodo: async');
     const originalTodo = todoState.todoList.find((todo) => todo.id === editedTodo.id);
     // Optimistic Strategy
-    /* Before reducer
-    const updatedTodos = todoList.map((todo) => {
-      if (todo.id === editedTodo.id) {
-        return { ...editedTodo };
-      } else {
-        return todo;
-      }
-    });
-    setTodoList([...updatedTodos]);
-    */
     dispatch({ type: todoActions.updateTodo, editedTodo: editedTodo });
     const payload = {
       records: [
@@ -173,36 +120,16 @@ function App() {
       body: JSON.stringify(payload),
     };
     try {
-      /* Before reducer
-      setIsSaving(true);
-      */
       dispatch({ type: todoActions.startRequest });
       const resp = await fetch(encodeUrl(sortField, sortDirection, queryString), options);
       if (!resp.ok) {
         throw new Error(resp.message);
       }
     } catch (error) {
-      /* Before reducer
-      console.error(error);
-      setErrorMessage(`${error.message}. Reverting todo...`);
-      */
       dispatch({ type: todoActions.setLoadError, error: error });
       // Optimistic Strategy
-      /* Before reducer
-      const revertedTodos = todoList.map((todo) => {
-        if (todo.id === originalTodo.id) {
-          return { ...originalTodo };
-        } else {
-          return todo;
-        }
-      });
-      setTodoList([...revertedTodos]);
-      */
       dispatch({ type: todoActions.revertTodo, originalTodo: originalTodo });
     } finally {
-      /* Before reducer
-      setIsSaving(false);
-      */
       dispatch({ type: todoActions.endRequest });
     }
   }
@@ -211,21 +138,6 @@ function App() {
     console.log('App: completeTodo: async');
     const originalTodo = todoState.todoList.find((todo) => todo.id === id);
     // Optimistic Strategy
-    /* Before reducer
-    const updatedTodos = todoList.map((todo) => {
-      if (todo.id === id) {
-        const updatedTodo = {
-          id: todo.id,
-          title: todo.title,
-          isCompleted: true
-        };
-        return { ...updatedTodo };
-      } else {
-        return todo;
-      }
-    });
-    setTodoList([...updatedTodos]);
-    */
     dispatch({ type: todoActions.completeTodo, id: id });
     const payload = {
       records: [
@@ -246,44 +158,21 @@ function App() {
       body: JSON.stringify(payload),
     };
     try {
-      /* Before reducer
-      setIsSaving(true);
-      */
       dispatch({ type: todoActions.startRequest });
       const resp = await fetch(encodeUrl(sortField, sortDirection, queryString), options);
       if (!resp.ok) {
         throw new Error(resp.message);
       }
     } catch (error) {
-      /* Before reducer
-      console.error(error);
-      setErrorMessage(`${error.message}. Reverting todo...`);
-      */
       dispatch({ type: todoActions.setLoadError, error: error });
       // Optimistic Strategy
-      /* Before reducer
-      const revertedTodos = todoList.map((todo) => {
-        if (todo.id === originalTodo.id) {
-          return { ...originalTodo };
-        } else {
-          return todo;
-        }
-      });
-      setTodoList([...revertedTodos]);
-      */
       dispatch({ type: todoActions.revertTodo, originalTodo: originalTodo });
     } finally {
-      /* Before reducer
-      setIsSaving(false);
-      */
       dispatch({ type: todoActions.endRequest });
     }
   }
 
   function cleanErrorMessage() {
-    /* Before reducer
-    setErrorMessage('');
-    */
     dispatch({ type: todoActions.clearError });
   }
 
